4df4c20320f2ba450f2ddbae1fb2984242e16f3f
Riyaz1203 <mohammedriyaz12032003@gmail.com>
2025-05-13 15:52:24 +0530

    All test API updated

--- src/pages/api/hotel-content/[id].ts
+++ src/pages/api/hotel-content/[id].ts
@@ -1,10 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   const { id } = req.query;
 
   if (!id || typeof id !== 'string') {
--- src/pages/api/hotels.ts
+++ src/pages/api/hotels.ts
@@ -1,10 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   try {
     const response = await axios.get(
       `https://api.stayflexi.com/core/api/v1/beservice/grouphotels?groupId=24316`,
